d49c8b283d2f0399cd4724430b8751f79d2bcd2a
KD丶珑 <2507086134@qq.com>
2021-06-04 21:11:21 +0800

    2021年06月04日21时11分21秒

--- js/收藏夹程序.js
+++ js/收藏夹程序.js
@@ -1,1 +1,2 @@
+//用于GitHub资源页的快速打开(jsdelivr)
 javascript:if(/^https?:\/\/github\.com\/.*\/.*\/blob\/master\/.*/.test(window.location.href)){var urlArr=window.location.href.split('/');var URL="https://cdn.jsdelivr.net/gh/"+urlArr[3]+"/"+urlArr[4];for(var i=7;i<urlArr.length;i++){URL=URL+"/"+urlArr[i]}window.open(URL)}else{console.log("不是GitHub资源页！！")}
