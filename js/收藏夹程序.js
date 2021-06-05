eb98b3dbbcb3969a091d9c37af68ed706fff334a
KD-happy <67636118+KD-happy@users.noreply.github.com>
2021-06-05 08:03:08 +0800

    Update 收藏夹程序.js

--- js/收藏夹程序.js
+++ js/收藏夹程序.js
@@ -1,2 +1,2 @@
 //用于GitHub资源页的快速打开(jsdelivr)
-javascript:if(/^https?:\/\/github\.com\/.*\/.*\/blob\/master\/.*/.test(window.location.href)){var urlArr=window.location.href.split('/');var URL="https://cdn.jsdelivr.net/gh/"+urlArr[3]+"/"+urlArr[4];for(var i=7;i<urlArr.length;i++){URL=URL+"/"+urlArr[i]}window.open(URL)}else{console.log("不是GitHub资源页！！")}
+javascript:if(/^https?:\/\/github\.com\/.*\/.*\/blob\/.*\/.*/.test(window.location.href)){var urlArr=window.location.href.split('/');var Tag=/^https?:\/\/github\.com\/.*\/.*\/blob\/(.*)\/.*/.exec(window.location.href)[1];var URL="https://cdn.jsdelivr.net/gh/"+urlArr[3]+"/"+urlArr[4]+"@"+Tag;for(var i=7;i<urlArr.length;i++){URL=URL+"/"+urlArr[i]}window.open(URL)}else{console.log("不是GitHub资源页！！")}
