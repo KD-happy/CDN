d84fae0244edfde62f7e92decc80f7aece2146b9
KD丶珑 <2507086134@qq.com>
2021-06-12 19:26:50 +0800

    2021年06月12日19时26分50秒

---
+++ get.js
@@ -0,0 +1,4 @@
+var getCookie = function(e) {
+	return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(e).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null
+}
+console.log('wwww');
