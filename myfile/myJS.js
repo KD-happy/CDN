97d60113a5d2b79237fe2d88689e0cc321e020e4
KD丶珑 <2507086134@qq.com>
2021-06-29 20:07:45 +0800

    2021年06月29日20时07分45秒

--- myfile/myJS.js
+++ myfile/myJS.js
@@ -1,3 +1,4 @@
+/******************** 添加外部链接资源到head中 ********************/
 //添加CSS到head中
 function addCSS(url) {
     var css = document.createElement('link');
@@ -12,6 +13,9 @@ function addJS(url) {
     js.src = url;
     document.head.appendChild(js);
 }
+
+
+/******************** 对网站的Cookie的操作 ********************/
 //通过Cookie的name获取Cookie的值
 function getCookie(e) {
     return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(e).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null
@@ -33,6 +37,24 @@ function getCookie(name) {
     }
     return ''
 }
+//此方法和上面的是一样的
+function getCookieValue(name){
+   let arr,reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg)) {
+        return unescape(arr[2]);
+    } else {
+        return null;
+    }
+}
+//设置Cookie的值
+function setCookie(cookiename, value){
+    let exp = new Date();
+    exp.setTime(exp.getTime() + 3*60*60*1000);
+    document.cookie = cookiename + "="+ escape (value) + "; path=/; expires=" + exp.toGMTString();
+}
+
+
+/******************** 其他好玩的功能 ********************/
 //复制
 function doCopy(content) {
     var aux = document.createElement("input"); 
@@ -41,4 +63,23 @@ function doCopy(content) {
     aux.select();
     document.execCommand("copy"); 
     document.body.removeChild(aux);
+}
+//延迟时间： await sleep(1000).then(函数)
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+//获得一个随机数
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+//网站通知：通过网站来调用的通知，在右下角
+function showMessageWindow(title, content, callback){
+    if(window.Notification && Notification.permission !== "denied") {
+        Notification.requestPermission(function(status) {
+            var notice_ = new Notification(title, { body: content });
+            notice_.onclick = function() {
+                callback();
+            }
+        });
+    }   
 }
