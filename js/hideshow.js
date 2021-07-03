d2553f4485be047b3d5f882a766b980608ef492d
KD丶珑 <2507086134@qq.com>
2021-07-03 12:17:06 +0800

    2021年07月03日12时17分06秒

--- js/hideshow.js
+++ js/hideshow.js
@@ -15,3 +15,23 @@ function ab(){
     });
 };
 setTimeout(ab, 1000);
+
+/*
+    绑定了快捷键：Ctrl+Space 播放/停止
+    Ctrl+Left 下一首
+    Ctrl+Right 上一首
+*/
+document.onkeydown = onKeyDown; //快捷键
+function onKeyDown () {
+    if(window.event.ctrlKey){
+        if(window.event.keyCode == 32) { //ctrl+space 切换播放
+            ap.toggle();
+        }
+        if(window.event.keyCode == 39) {
+            ap.skipForward();
+        }
+        if(window.event.keyCode == 37) {
+            ap.skipBack();
+        }
+    }
+};
